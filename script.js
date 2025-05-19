74538f938bab1e44afeb67ef613c79782a02b804
Araz Gholami <heshzad@gmail.com>
2025-05-19 11:24:32 +0330

    Make Tasks ID unique

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ async function saveData() {
 }
 
 function generateId() {
-    return Date.now().toString(36) + Math.random().toString(36).substr(2, 5);
+    return uuid.v4();
 }
 
 function renderCategories() {
